0664444696ca62ef6bb83542824a8f7da055b8a1
Alexandra Salas <69660054+aesalas@users.noreply.github.com>
2023-01-25 22:48:57 -0500

    conact form styling

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,52 +1,34 @@
-import {useState} from 'react'
 
-const encode = (data) => {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
-}
-
-export default function Form (){
-  const [state, setState] = useState({name: '', email: '', message: '' })
-
-  const handleChange = e =>
-    setState({...state, [e.target.name]: e.target.value })
-
-  const handleSubmit = e => {
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'contactForm', ...state })
-    })
-      .then(() => alert('Success!'))
-      .catch(error => alert(error))
-    e.preventDefault()
-  }
-
+export default function Form() {
   return (
-    <form 
-      className='contactForm' 
-      onSubmit={handleSubmit}>
-
+    <div className="contact">
+      <form 
+        method='POST' 
+        name='contactform' 
+        className='contactForm'>
       <input 
-        type='text' 
-        name='name' 
-        value={state.name}
-        placeholder='Enter your name'
-        onChange={handleChange} />
+        type='hidden' 
+        name='form-name'
+        value='contactForm' />
 
-      <input 
-        type='email' 
-        name='email' 
-        value={state.email}
-        placeholder='Enter your email'
-        onChange={handleChange} />
 
-      <textarea 
-        name='message' 
-        placeholder='Messaage'
-        value={state.message}
-        onChange={handleChange}></textarea>
-      <button type='submit'>Submit</button>
-    </form>
+        <input 
+          type='text' 
+          name='name' 
+          placeholder='Enter your name' />
+
+        <input 
+          type='email' 
+          name='email' 
+          placeholder='Enter your email' />
+
+        <textarea 
+          name='message' 
+          placeholder='Message'></textarea>
+
+        <button type='submit'>Submit</button>
+
+      </form>
+    </div>
   )
 }
