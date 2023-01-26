df8245350a75cd3f7c6b41c4476bd792c2fede89
Alexandra Salas <69660054+aesalas@users.noreply.github.com>
2023-01-25 22:37:49 -0500

    update form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,30 +1,48 @@
 
 export default function Form() {
   return (
-    <form 
-      method='POST' 
-      name='contactform' 
-      className='contactForm'>
-<input 
-  type='hidden' 
-  name='form-name'
-  value='contactForm' />
+    <div className="contact">
+      <form 
+        method='POST' 
+        name='contactform' 
+        className='contactForm'>
       <input 
-        type='text' 
-        name='name' 
-        placeholder='Enter your name' />
+        type='hidden' 
+        name='form-name'
+        value='contactForm' />
+        <div class="row">
+          <div class="col-25">
+            <label for="name">Name: </label>
+          </div>
+          <div class="col-75">
+            <input 
+                type='text' 
+                name='name' 
+                placeholder='Enter your name' />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-25">
+            <label for="email">Email</label>
+          </div>
+          <div class="col-75">
+            <input 
+                type='email' 
+                name='email' 
+                placeholder='Enter your email' />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-25">
+            <label for="subject">Message</label>
+          </div>
+          <div class="col-75">
+            <textarea id="message" name="message" placeholder="Write something.." style="height:200px"></textarea>
+          </div>
+        </div>
+        <button type='submit'>Submit</button>
 
-      <input 
-        type='email' 
-        name='email' 
-        placeholder='Enter your email' />
-
-      <textarea 
-        name='message' 
-        placeholder='Message'></textarea>
-
-      <button type='submit'>Submit</button>
-
-    </form>
+      </form>
+    </div>
   )
 }
