2748f00f68d33665f2a7bb6092e02df664978fae
Alexandra Salas <69660054+aesalas@users.noreply.github.com>
2023-01-25 22:07:58 -0500

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,47 +1,112 @@
 import React, { useState } from "react";
+import emailjs from 'emailjs-com';
 import { useRef } from 'react';
-import { Container, Card, TextField} from "react-bootstrap";
+import { Container, Row, Col} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import { Form, Input, TextArea } from 'semantic-ui-react';
+import Alert from 'react-bootstrap/Alert';
 
 
 
-export default function ContactForm() {
-    const [success, setSuccess] = useState(false);
+function ContactForm() { 
+ const form = useRef();
+ {/* <!-- email logic and message sent/failed alert --> */}
+    const [show, setShow] = useState(false);
+    const [emSent, setEmSent] = useState(false);
 
-    useEffect(() => {
-        if (window.location.search.includes('success=true')) {
-            setSuccess(true);
-        }
-    }, []);
+    if (show && emSent) {
+      return (
+        <Alert variant="success" onClose={() => setShow(false)} dismissible>
+          <Alert.Heading>Message Sent</Alert.Heading>
+          <p>
+            Thank you for reaching out! I'll get back to you as soon as possible.
+          </p>
+          <p>
+            - Alex
+          </p>
+        </Alert>
+      );
 
-    return (
-        <Container maxWidth="sm">
-            <h2>CONTACT</h2>
-            {success && (
-                <p style={{ color: "green" }}>Thanks for your message! </p>
-            )}
-            <Card>
-                <form
-                    name="contact"
-                    method="POST"
-                    action="/contact/?success=true"
-                    data-netlify="true"
-                >
-                    <input type="hidden" name="form-name" value="contact" />
-                    <TextField id="standard-basic" label="name" name="name" /> <br />
-                    <TextField id="standard-basic" label="email" name="email" /> <br />
-                    <TextField
-                        multiline
-                        id="standard-multiline-static"
-                        label="message"
-                        name="message"
-                    />
-                    <br />
-                    <div>
-                        <Button type="submit">Send</Button>
-                    </div>
-                </form>
-            </Card>
-        </Container>
-    );
+    } 
+    if (show && !emSent) {
+      return (
+        <Alert variant="danger" onClose={() => setShow(false)} dismissible>
+          <Alert.Heading>Something went wrong</Alert.Heading>
+          <p>
+            I apologize for the inconvenience, please try again.
+          </p>
+          <p>
+            If the error keeps occuring reach out to me via 
+            Linkedin and I'll be happy to message you there!
+          </p>
+        </Alert>
+      );
+    }
+    const sendEmail = (e) => {
+      e.preventDefault();
+
+      emailjs.sendForm('service_dpkpiyd', 'template_vg7lyf7', e.target, 'xIGNAR0gHYEXzl1pb')
+        .then((result) => {
+          console.log(result.text);
+          setEmSent(true);
+          setShow(true);
+
+        }, (error) => {
+          console.log(error.text);
+          setEmSent(false);
+          setShow(true);
+        });
+      e.target.reset()
+    };
+
+  return (
+    <Container >
+      <Row>
+        <Col>
+          {/* <!-- contact form --> */}
+          <div className="contact">
+            <Form ref={form} onSubmit={sendEmail}>
+              <Form.Field
+                id='name'
+                control={Input}
+                label='Name'
+                name='name'
+                placeholder='Name…'
+                required
+                icon='user circle'
+                iconPosition='left'
+              />
+              <Form.Field
+                id='Email'
+                control={Input}
+                label='Email'
+                name='email'
+                placeholder='Email…'
+                required
+                icon='mail'
+                iconPosition='left'
+              />
+
+              <Form.Field
+                id='message'
+                control={TextArea}
+                label='Message'
+                name='message'
+                placeholder='Message…'
+                required
+              />
+              <div className="submit-btn">
+              <Button
+                variant="primary"
+                target="_blank"
+                style={{ maxWidth: "250px" }}
+              >Submit</Button>
+              </div>
+            </Form>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
+export default ContactForm;
