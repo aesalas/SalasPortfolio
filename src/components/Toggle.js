aefaad0ad510d9c48449b67e21be27f9c42553a6
Alexandra Salas <69660054+aesalas@users.noreply.github.com>
2023-01-30 15:15:30 -0500

    toggle button styling

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -43,10 +43,20 @@ function Toggle() {
 
     return (
       <div className="container--toggle">
-        <input aria-label="dark mode toggle" role="switch" aria-checked={ariaActive} onKeyPress={handleKeypress} type="checkbox" id="toggle" className="toggle--checkbox" onClick={handleOnClick} checked={active} readOnly />
+        <input  aria-label="dark mode toggle" 
+                role="switch" 
+                aria-checked={ariaActive} 
+                onKeyPress={handleKeypress} 
+                type="checkbox" 
+                id="toggle" 
+                className="toggle--checkbox" 
+                onClick={handleOnClick} 
+                checked={active} 
+                readOnly />
         <label htmlFor="toggle" className="toggle--label">
           <span className="toggle--label-background"></span>
         </label>
+        
       </div>
     )
 }
