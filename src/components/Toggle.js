574ef495c9a0d8d92cfbee4135a17b58432515f9
Alexandra Salas <69660054+aesalas@users.noreply.github.com>
2023-01-30 20:34:13 -0500

    toggle v2

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,45 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import './Toggle.css';
+import '../styles/toggle.css';
 import { setTheme } from '../utils/themes';
-
 function Toggle() {
-    // false = dark mode because of the way I wrote the CSS
-    const [active, setActive] = useState(false)
-    // the opposite, for screenreaders
-    const [ariaActive, setAriaActive] = useState(false)
-    let theme = localStorage.getItem('theme')
-
-    function setTheme(themeName) {
-      localStorage.setItem('theme', themeName);
-      document.documentElement.className = themeName;
-  }
-
-  // function to toggle between light and dark theme
-  function toggleTheme() {
-      if (localStorage.getItem('theme') === 'theme-dark') {
-          setTheme('theme-light');
-      } else {
-          setTheme('theme-dark');
-      }
-  }
-
-  // Immediately invoked function to set the theme on initial load
-  (function () {
-      if (localStorage.getItem('theme') === 'theme-dark') {
-          setTheme('theme-dark');
-      } else {
-          setTheme('theme-light');
-      }
-
-    return (
-      <div className="container--toggle">
-        <input type="checkbox" id="toggle" class="toggle--checkbox" />
-        <label for="toggle" class="toggle--label">
-          <span class="toggle--label-background"></span>
-        </label>
-        
-      </div>
+    const [togClass, setTogClass] = useState('dark');
+    let theme = localStorage.getItem('theme');
+const handleOnClick = () => {
+        if (localStorage.getItem('theme') === 'theme-dark') {
+            setTheme('theme-light');
+            setTogClass('light')
+        } else {
+            setTheme('theme-dark');
+            setTogClass('dark')
+        }
+    }
+useEffect(() => {
+        if (localStorage.getItem('theme') === 'theme-dark') {
+            setTogClass('dark')
+        } else if (localStorage.getItem('theme') === 'theme-light') {
+            setTogClass('light')
+        }
+    }, [theme])
+return (
+        <div className="container--toggle">
+            {
+                togClass === "light" ?
+                <input type="checkbox" id="toggle" className="toggle--checkbox" onClick={handleOnClick} checked />
+                :
+                <input type="checkbox" id="toggle" className="toggle--checkbox" onClick={handleOnClick} />
+            }
+            <label htmlFor="toggle" className="toggle--label">
+                <span className="toggle--label-background"></span>
+            </label>
+        </div>
     )
 }
-
 export default Toggle;
